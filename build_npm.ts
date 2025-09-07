e3a50c71e38c5b9e186c4f0f48c593858312cd8d
marmooo <marmooo@users.noreply.github.com>
2025-09-07 17:38:40 +0900

    0.0.1

--- build_npm.ts
+++ build_npm.ts
@@ -11,7 +11,7 @@ await build({
   },
   package: {
     name: "emoji-particle",
-    version: "0.0.0",
+    version: "0.0.1",
     description: "Fireworks and popcorn particle effects using emojis.",
     license: "MIT",
     main: "emoji-particle.ts",
